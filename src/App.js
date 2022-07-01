425aa7801644bc1e3c2cec5f51d70eb03829a317
esadakman <esadd26@gmail.com>
2022-07-02 00:01:37 +0300

    jsx finished

--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
 import "./App.css";
 import Cards from "./components/Cards";
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-const url = "https://randomuser.me/api/";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState([]);
-
-  const users = async () => {
-    try {
-      const { data } = await axios.get(url);
-      setUser(data.results[0]);
-      // setLoading(false);
-    } catch (error) {
-      alert(error);
-    }
-  };
-
-  useEffect(() => {
-    users();
-  }, []);
-
-  // if (loading) {
-  //   return <h2>Loading...</h2>;
-  // }
   return (
     <div className="App">
-      <Cards user={user}></Cards>
+      <Cards />
 
-      <div class="buttons">
-        <a href="${html_url}" target="_blank">
+      {/* <div className="buttons">
+        <a href="html_url" target="_blank">
           Random User
         </a>
-        {/* <a href="${html_url}" target="_blank">
+        <a href="${html_url}" target="_blank">
           Visit Profile
-        </a> */}
-      </div>
+        </a>
+      </div> */}
     </div>
   );
 }
 
 export default App;
+
+// const [loading, setLoading] = useState(true);
+// const [user, setUser] = useState([]);
+
+// useEffect(() => {
+//   const users = async () => {
+//     try {
+//       const { data } = await axios.get(url);
+//       setUser(data.results[0]);
+//       console.log(data);
+//     } catch (error) {
+//       alert(error);
+//     }
+//   };
+// }, []);
+// const [loading, setLoading] = useState(true);
+// const [user, setUser] = useState([]);
+
+// const users = async () => {
+//   try {
+//     const { data } = await axios.get(url);
+//     setUser(data.results[0]);
+//     // setLoading(false);
+//   } catch (error) {
+//     alert(error);
+//   }
+// };
+
+// useEffect(() => {
+//   users();
+// }, []);
+
+// const response = await axios.get(url).then((res) => {
+//   const responseUsers = res.data;
+//   setUsers(responseUsers);
+//   console.log(res.data);
+// });
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,43 +1,64 @@
 import "./Card.css";
-// import { FaUser, FaSearchLocation, FaPhone, FaMailBulk } from "react-icons/fa";
-const Cards = () => {
-  // const Cards = ({ user }) => {
-  // const {
-  // picture: { medium },
-  // name: { first, last },
-  // email,
-  // cell,
-  // location: { city, country },
-  // dob: { age },
-  // registered: { date },
-  // } = user;
-  // console.log(user);
-  return (
-    <div class="card-container">
-      <span class="pro">Github</span>
-      <img
-        class="round"
-        src="https://avatars.githubusercontent.com/u/98649983?v=4"
-        alt="user"
-      />
-      <div>
-        <h2 class="name">
-          <span>name</span>
-        </h2>
-        <h5 class="email">email</h5>
-        <h5 class="tel">tel</h5>
-      </div>
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+const url = "https://randomuser.me/api/";
 
-      <div class="infos">
-        <div class="location">
-          <h4>Location:</h4>
-          <p>New Zealand Invercargill</p>
-        </div>
-        <div class="followers">
-          <h4>Register Date:</h4>
-          <p>2010-05-05</p>
-        </div>
-      </div>
+const Cards = () => {
+  const [users, setUsers] = useState([]);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setUsers(res.data.results);
+    });
+  }, [count]);
+
+  return (
+    <div className="card-container">
+      <span className="pro">Github</span>
+      {users.map((user, index) => {
+        const {
+          picture: { large },
+          name: { first, last },
+          email,
+          cell,
+          location: { city, country },
+          // dob: { age },
+          registered: { date },
+        } = user;
+        return (
+          <div key={index}>
+            <img className="round" src={large} alt="user" />
+            <div>
+              <h2 className="name">
+                <span>
+                  {first} {last}
+                </span>
+              </h2>
+              <h5 className="email">{email}</h5>
+              <h5 className="cell">{cell}</h5>
+            </div>
+            <div className="infos">
+              <div className="location">
+                <h4>Location:</h4>
+                <p>
+                  {country} {city}
+                </p>
+              </div>
+              <div className="followers">
+                <h4>Register Date:</h4>
+                <p>{date.toString()}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      {/* <div className="buttons"  >
+        <a href="html_url" target="_blank">
+          Random User
+        </a>
+      </div> */}
+      <button onClick={() => setCount(count + 1)}>Click</button>
     </div>
   );
 };
